5b4ee676b64f5ddac904888ee5670511be6e3b90
culley-aidan <culley-aidan@users.noreply.github.com>
2019-10-24 09:47:47 -0500

    Update PayrollGUI.Designer.cs

--- PayrollGUI/PayrollGUI/PayrollGUI.Designer.cs
+++ PayrollGUI/PayrollGUI/PayrollGUI.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.lblName = new System.Windows.Forms.Label();
+            this.lblSSN = new System.Windows.Forms.Label();
+            this.lblPayRate = new System.Windows.Forms.Label();
+            this.lblHourlyRate = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblName
@@ -40,11 +43,41 @@
             this.lblName.TabIndex = 0;
             this.lblName.Text = "Enter your name: ";
             // 
+            // lblSSN
+            // 
+            this.lblSSN.AutoSize = true;
+            this.lblSSN.Location = new System.Drawing.Point(13, 30);
+            this.lblSSN.Name = "lblSSN";
+            this.lblSSN.Size = new System.Drawing.Size(83, 13);
+            this.lblSSN.TabIndex = 1;
+            this.lblSSN.Text = "Enter your SSN:";
+            // 
+            // lblPayRate
+            // 
+            this.lblPayRate.AutoSize = true;
+            this.lblPayRate.Location = new System.Drawing.Point(13, 47);
+            this.lblPayRate.Name = "lblPayRate";
+            this.lblPayRate.Size = new System.Drawing.Size(104, 13);
+            this.lblPayRate.TabIndex = 2;
+            this.lblPayRate.Text = "Enter hourly payrate:";
+            // 
+            // lblHourlyRate
+            // 
+            this.lblHourlyRate.AutoSize = true;
+            this.lblHourlyRate.Location = new System.Drawing.Point(13, 64);
+            this.lblHourlyRate.Name = "lblHourlyRate";
+            this.lblHourlyRate.Size = new System.Drawing.Size(102, 13);
+            this.lblHourlyRate.TabIndex = 3;
+            this.lblHourlyRate.Text = "Enter hours worked:";
+            // 
             // PayrollGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblHourlyRate);
+            this.Controls.Add(this.lblPayRate);
+            this.Controls.Add(this.lblSSN);
             this.Controls.Add(this.lblName);
             this.Name = "PayrollGUI";
             this.Text = "Payroll";
@@ -56,6 +89,9 @@
         #endregion
 
         private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblSSN;
+        private System.Windows.Forms.Label lblPayRate;
+        private System.Windows.Forms.Label lblHourlyRate;
     }
 }
 
