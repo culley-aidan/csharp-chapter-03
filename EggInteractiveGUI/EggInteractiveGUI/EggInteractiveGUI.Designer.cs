ec2a103a4739b42af8323f816c6cdf1568c7bb57
culley-aidan <culley-aidan@users.noreply.github.com>
2019-10-24 09:30:12 -0500

    created chicken text boxes

--- EggInteractiveGUI/EggInteractiveGUI/EggInteractiveGUI.Designer.cs
+++ EggInteractiveGUI/EggInteractiveGUI/EggInteractiveGUI.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             this.lblTitle = new System.Windows.Forms.Label();
+            this.txtChickenOne = new System.Windows.Forms.TextBox();
+            this.txtChickenTwo = new System.Windows.Forms.TextBox();
+            this.txtChickenThree = new System.Windows.Forms.TextBox();
+            this.txtChickenFour = new System.Windows.Forms.TextBox();
+            this.txtChickenFive = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lblTitle
@@ -40,11 +45,51 @@
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Enter eggs produced by each of 5 chickens";
             // 
+            // txtChickenOne
+            // 
+            this.txtChickenOne.Location = new System.Drawing.Point(16, 54);
+            this.txtChickenOne.Name = "txtChickenOne";
+            this.txtChickenOne.Size = new System.Drawing.Size(58, 20);
+            this.txtChickenOne.TabIndex = 1;
+            // 
+            // txtChickenTwo
+            // 
+            this.txtChickenTwo.Location = new System.Drawing.Point(91, 54);
+            this.txtChickenTwo.Name = "txtChickenTwo";
+            this.txtChickenTwo.Size = new System.Drawing.Size(58, 20);
+            this.txtChickenTwo.TabIndex = 2;
+            // 
+            // txtChickenThree
+            // 
+            this.txtChickenThree.Location = new System.Drawing.Point(169, 54);
+            this.txtChickenThree.Name = "txtChickenThree";
+            this.txtChickenThree.Size = new System.Drawing.Size(58, 20);
+            this.txtChickenThree.TabIndex = 3;
+            // 
+            // txtChickenFour
+            // 
+            this.txtChickenFour.Location = new System.Drawing.Point(54, 80);
+            this.txtChickenFour.Name = "txtChickenFour";
+            this.txtChickenFour.Size = new System.Drawing.Size(58, 20);
+            this.txtChickenFour.TabIndex = 4;
+            // 
+            // txtChickenFive
+            // 
+            this.txtChickenFive.Location = new System.Drawing.Point(131, 80);
+            this.txtChickenFive.Name = "txtChickenFive";
+            this.txtChickenFive.Size = new System.Drawing.Size(58, 20);
+            this.txtChickenFive.TabIndex = 5;
+            // 
             // EggInteractiveGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtChickenFive);
+            this.Controls.Add(this.txtChickenFour);
+            this.Controls.Add(this.txtChickenThree);
+            this.Controls.Add(this.txtChickenTwo);
+            this.Controls.Add(this.txtChickenOne);
             this.Controls.Add(this.lblTitle);
             this.Name = "EggInteractiveGUI";
             this.Text = "Egg Interactive";
@@ -56,6 +101,11 @@
         #endregion
 
         private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtChickenOne;
+        private System.Windows.Forms.TextBox txtChickenTwo;
+        private System.Windows.Forms.TextBox txtChickenThree;
+        private System.Windows.Forms.TextBox txtChickenFour;
+        private System.Windows.Forms.TextBox txtChickenFive;
     }
 }
 
