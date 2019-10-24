d8a2e0e136aef7990179cccc163e913bebd47878
culley-aidan <culley-aidan@users.noreply.github.com>
2019-10-24 09:29:03 -0500

    created lblTitle

--- EggInteractiveGUI/EggInteractiveGUI/EggInteractiveGUI.Designer.cs
+++ EggInteractiveGUI/EggInteractiveGUI/EggInteractiveGUI.Designer.cs
@@ -28,20 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblTitle = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(13, 13);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(214, 13);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Enter eggs produced by each of 5 chickens";
             // 
             // EggInteractiveGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTitle);
             this.Name = "EggInteractiveGUI";
             this.Text = "Egg Interactive";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
     }
 }
 
